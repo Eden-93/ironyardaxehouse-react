49d554e2a8c454774305d69f4c348020b4999c45
Eden Ryan <eden.ryan@rocketmail.com>
2020-06-29 22:03:51 -0400

    Images

---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Directory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            events: [
+                {
+                    id: 0,
+                    name: 'Birthday Parties',
+                    image: '../assets/image/birthday parties.JPG',
+                    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum ea autem quibusdam eius itaque quis praesentium voluptates nulla. Aut consectetur et modi corrupti consequatur! Et, voluptatum placeat! Eius, repellat fuga."
+                },
+                {
+                    id: 1,
+                    name: 'Festivals',
+                    image: 'assets/image/Beer Bournon sceenshot.png',
+                    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum ea autem quibusdam eius itaque quis praesentium voluptates nulla. Aut consectetur et modi corrupti consequatur! Et, voluptatum placeat! Eius, repellat fuga."
+                },
+                {
+                    id: 2,
+                    name: 'Work Events',
+                    image: '../public../assets/image/treinanniversary.JPG',
+                    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum ea autem quibusdam eius itaque quis praesentium voluptates nulla. Aut consectetur et modi corrupti consequatur! Et, voluptatum placeat! Eius, repellat fuga."
+                }
+            ]
+        };
+    }
+
+    render() {
+        const directory = this.state.events.map(event => {
+            return (
+                <div key={event.id} className="col">
+                    <img src={event.image} alt={event.name} />
+                    <h2>{event.name}</h2>
+                    <p>{event.description}</p>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {directory}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Directory;
