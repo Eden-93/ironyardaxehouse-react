d69516832399a9081065a76cec5ce28df6f65454
Eden Ryan <eden.ryan@rocketmail.com>
2020-07-28 13:03:21 -0400

    Functional components

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 
-class Directory extends Component {
-    
-    render() {
-        const directory = this.props.events.map(event => {
-            return (
-                <div key={event.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(event.id)}>
+function RenderDirectoryItem({event, onClick}) {
+    return (
+        <Card onClick={() => onClick(event.id)}>
                         <CardImg width="100%" src={event.image} alt={event.name} />
                         <CardImgOverlay>
                         <CardTitle>{event.name}</CardTitle>
                         </CardImgOverlay>
                     </Card>
-                </div>
-            );
-        });
+    )
+}
 
+function Directory(props) {
+    
+    const directory = props.events.map(event => {
         return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
+            <div key={event.id} className="col-md-5 m-1">
+                <RenderDirectoryItem event={event} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
 }
 
 export default Directory;
--- src/components/EventInfoComponent.js
+++ src/components/EventInfoComponent.js
@@ -1,36 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class EventInfo extends Component {
 
-    renderEvent(event){
-        return(
-            <div className="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={event.image} alt={event.name}/>
-                    <CardBody>
-                        <CardTitle>{event.name}</CardTitle>
-                        <CardText>{event.description}</CardText>
-                    </CardBody>
-                </Card>
+function RenderEvent({ event }) {
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={event.image} alt={event.name} />
+                <CardBody>
+                    <CardTitle>{event.name}</CardTitle>
+                    <CardText>{event.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+function EventInfo(props) {
+    if (props.event) {
+        return (
+            <div className="container" >
+                <div className="row" >
+                    <RenderEvent event={props.event} />
+                </div>
             </div>
         );
     }
-
-    render(){
-        if (this.props.event) {
-            return (
-            <div className="container" >
-                <div className="row" >
-                    {this.renderEvent(this.props.event)}
-                </div>
-            </div>
-            );
-        }
-            return (
-                <div />
-            );
-    }
+    return (
+        <div />
+    );
 }
+
 
 export default EventInfo;
